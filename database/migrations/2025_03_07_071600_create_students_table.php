d9c1325bcfd5f7c5d01990f6749542363dfc5f62
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2025-03-07 12:34:43 +0330

    add migration

---
+++ database/migrations/2025_03_07_071600_create_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Classes;
+use App\Models\Section;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+
+            $table->foreignIdFor(Classes::class, 'class_id');
+            $table->foreignIdFor(Section::class, 'section_id');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
