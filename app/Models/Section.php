2ab5a16ca05399b31939b05451c05dcfe60cf606
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2025-03-07 12:20:24 +0330

    complite the models

---
+++ app/Models/Section.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Section extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'class_id',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'class_id' => 'integer',
+    ];
+
+    public function class(): BelongsTo
+    {
+        return $this->belongsTo(Classes::class);
+    }
+
+    public function students(): HasMany
+    {
+        return $this->hasMany(Student::class);
+    }
+}
