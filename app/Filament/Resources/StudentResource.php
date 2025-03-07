2a3da8c99e1c39f21f1629fde925f15583fe29e2
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2025-03-07 13:46:49 +0330

    complite student resource

--- app/Filament/Resources/StudentResource.php
+++ app/Filament/Resources/StudentResource.php
@@ -4,11 +4,18 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\StudentResource\Pages;
 use App\Filament\Resources\StudentResource\RelationManagers;
+use App\Models\Classes;
+use App\Models\Section;
 use App\Models\Student;
 use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Forms\Get;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\Filter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -19,22 +26,37 @@ class StudentResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static ?string $navigationGroup = "Academic Management";
+
+    public static function getNavigationBadge(): ?string
+    {
+        return static::$model::count();
+    }
+
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('email')
-                    ->email()
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\Select::make('class_id')
-                    ->relationship('class', 'name')
+                Select::make('class_id')
+                    ->live()
+                    ->relationship(name: 'class', titleAttribute: 'name'),
+                Select::make('section_id')
+                    ->label('Section')
+                    ->options(function (Get $get) {
+                        $classId = $get('class_id');
+
+                        if ($classId) {
+                            return Section::where('class_id', $classId)->pluck('name', 'id')->toArray();
+                        }
+                    }),
+                TextInput::make('name')
+                    ->autofocus()
                     ->required(),
-                Forms\Components\Select::make('section_id')
-                    ->relationship('section', 'name')
+                TextInput::make('email')
+                    ->unique()
+                    ->required(),
+                TextInput::make('password')
+                    ->password()
                     ->required(),
             ]);
     }
@@ -43,28 +65,48 @@ class StudentResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('name')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('email')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('class.name')
-                    ->numeric()
+                TextColumn::make('name')
+                    ->searchable()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('section.name')
-                    ->numeric()
+                TextColumn::make('email')
+                    ->searchable()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('class.name')
+                    ->badge()
+                    ->searchable(),
+                TextColumn::make('section.name')
+                    ->badge(),
             ])
             ->filters([
-                //
+                Filter::make('class-section-filter')
+                    ->form([
+                        Select::make('class_id')
+                            ->label('Filter By Class')
+                            ->placeholder('Select a Class')
+                            ->options(
+                                Classes::pluck('name', 'id')->toArray(),
+                            ),
+                        Select::make('section_id')
+                            ->label('Filter By Section')
+                            ->placeholder('Select a Section')
+                            ->options(function (Get $get) {
+                                $classId = $get('class_id');
+                                if ($classId) {
+                                    return Section::where('class_id', $classId)
+                                        ->pluck('name', 'id')
+                                        ->toArray();
+                                }
+                            }),
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query->when($data['class_id'], function ($query) use ($data) {
+                            return $query->where('class_id', $data['class_id']);
+                        })->when($data['section_id'], function ($query) use ($data) {
+                            return $query->where('section_id', $data['section_id']);
+                        });
+                    }),
             ])
+
             ->actions([
                 Tables\Actions\EditAction::make(),
             ])
